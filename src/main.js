904daf8123cf80beb71cc819124166aa7c9d7654
kurage14 <ting020610@gmail.com>
2025-12-02 17:28:51 +0800

    修改Devtools檢查權限

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import './style.css'
 import App from './App.vue'
 import router from './routes/router'
 import { createPinia } from 'pinia'
-createApp(App).use(createPinia()).use(router).mount('#app')
+const app = createApp(App)
+app.config.devtools = true;
+app.use(createPinia()).use(router).mount('#app')
